2f88d1e10d9e41e68c7f252f3e14b6447d9cb0c0
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2022-11-04 16:20:57 -0300

    promises 123

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -4,7 +4,90 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+// <----- VERSIÓN FUNCIÓN -----> //
 
+// const $Promise = function(executor) {
+//    if (!executor || typeof executor !== 'function') {
+//       throw TypeError(/executor.+function/i);
+//    }
+//    this._state = 'pending';
+//    this._handlerGroups = [{successCb: () => {}, errorCb: () => {}}]
+//    executor((value) => this._internalResolve(value), (reason) => this._internalReject(reason))
+// }
+
+// $Promise.prototype._internalResolve = function(value) {
+//    if (this._state === 'pending') {
+//       this._value = value;
+//       this._state = 'fulfilled'
+//    }
+// }
+// $Promise.prototype._internalReject = function(reason) {
+//    if (this._state === 'pending') {
+//       this._value = reason;
+//       this._state = 'rejected'
+//    }
+// }
+
+// $Promise.prototype.then = function(onfulfilled, onreject) {
+//       if (typeof onfulfilled === 'function' && typeof onreject === 'function')  {
+//          this._handlerGroups.push({successCb: onfulfilled, errorCb: onreject})
+//       } else  this._handlerGroups.push({successCb: false, errorCb: false})
+//    }   
+
+// <----- VERSIÓN CLASE -----> //
+class $Promise {
+   constructor(executor) {
+      if (!executor || typeof executor !== 'function') throw TypeError(/executor.+function/i);
+      
+      this._state = 'pending';
+      this._value = undefined;
+      this.executor = executor;
+      this._handlerGroups = []
+
+      this.executor((value) => this._internalResolve(value), (reason) => this._internalReject(reason))
+   }
+
+   _internalResolve(value) {
+      if (this._state === 'pending') {
+         this._value = value;
+         this._state = 'fulfilled'
+      }
+      this._callHandlers()
+   }
+
+   _internalReject(reason) {
+      if (this._state === 'pending') {
+         this._value = reason;
+         this._state = 'rejected'
+      }
+      this._callHandlers()
+   }
+
+   _callHandlers() {
+      while (this._handlerGroups.length) {
+         const currentHandler = this._handlerGroups.shift();
+         if (this._state === 'fulfilled' && currentHandler.successCb)
+            currentHandler.successCb(this._value)
+         
+         if ( this._state === 'rejected' && currentHandler.errorCb)
+            currentHandler.errorCb(this._value)
+      }
+   }
+
+   then(onfulfilled, onreject) {
+      const handlerGroup = {
+         successCb: typeof onfulfilled === 'function' ? onfulfilled : false,
+         errorCb: typeof onreject === 'function'? onreject : false
+      }
+      
+      this._handlerGroups.push(handlerGroup)
+      if  (this._state !== 'pending') this._callHandlers()
+   }   
+
+   catch(onreject) {
+      return this.then(null, onreject)
+   }
+}
 
 module.exports = $Promise;
 /*-------------------------------------------------------
